c4d73c16f6f788857195c332dc23cb73b1d5d1e0
Nguyen Quang <nqoptik@gmail.com>
2019-05-29 21:27:17 +0800

    Draw a cicle using midpoint algorithm C++

--- draw_circle/scripts/draw_circle.py
+++ draw_circle/scripts/draw_circle.py
@@ -2,7 +2,7 @@
 
 """!@file draw_circle.py
 @author Nguyen Quang <nguyenquang.emailbox@gmail.com>
-@brief The Circle drawing algorithms in computer graphics.
+@brief The circle drawing algorithms in computer graphics.
 @since 0.0.1
 @copyright Copyright (c) 2015, Nguyen Quang, all rights reserved.
 """
@@ -14,44 +14,44 @@ from pyglet.gl import *
 window = pyglet.window.Window()
 
 
-def draw_circle_midpoint(x, y, r):
-    """!@brief Draw a circle using midpoint algorithm.
-    @param[in] x The x-coordiante of the circle centre.
-    @param[in] y The y-coordiante of the circle centre.
+def draw_circle_midpoint(x_c, y_c, r):
+    """!@brief Draw a circle using the midpoint algorithm.
+    @param[in] x_c The x-coordiante of the circle centre.
+    @param[in] y_c The y-coordiante of the circle centre.
     @param[in] r The radius of the circle.
     @since 0.0.1
     """
 
     vertices = []
     p = float(5)/4 - r
-    x_ = 0
-    y_ = r
+    x = 0
+    y = r
 
-    vertices += [x + x_, y + y_]
-    vertices += [x + y_, y + x_]
-    vertices += [x + y_, y - x_]
-    vertices += [x + x_, y - y_]
-    vertices += [x - x_, y - y_]
-    vertices += [x - y_, y - x_]
-    vertices += [x - y_, y + x_]
-    vertices += [x - x_, y + y_]
+    vertices += [x_c + x, y_c + y]
+    vertices += [x_c + y, y_c + x]
+    vertices += [x_c + y, y_c - x]
+    vertices += [x_c + x, y_c - y]
+    vertices += [x_c - x, y_c - y]
+    vertices += [x_c - y, y_c - x]
+    vertices += [x_c - y, y_c + x]
+    vertices += [x_c - x, y_c + y]
 
-    while x_ < y_:
+    while x < y:
         if p < 0:
-            p = p + 2*x_ + 3
+            p = p + 2*x + 3
         else:
-            p = p + 2*x_ - 2*y_ + 5
-            y_ = y_ - 1
+            p = p + 2*x - 2*y + 5
+            y = y - 1
 
-        x_ = x_ + 1
-        vertices += [x + x_, y + y_]
-        vertices += [x + y_, y + x_]
-        vertices += [x + y_, y - x_]
-        vertices += [x + x_, y - y_]
-        vertices += [x - x_, y - y_]
-        vertices += [x - y_, y - x_]
-        vertices += [x - y_, y + x_]
-        vertices += [x - x_, y + y_]
+        x = x + 1
+        vertices += [x_c + x, y_c + y]
+        vertices += [x_c + y, y_c + x]
+        vertices += [x_c + y, y_c - x]
+        vertices += [x_c + x, y_c - y]
+        vertices += [x_c - x, y_c - y]
+        vertices += [x_c - y, y_c - x]
+        vertices += [x_c - y, y_c + x]
+        vertices += [x_c - x, y_c + y]
 
     return vertices
 
@@ -60,18 +60,18 @@ def draw_circle_midpoint(x, y, r):
 def on_draw():
     glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
 
-    x = 300
-    y = 200
+    x_c = 300
+    y_c = 200
     r = 150
     iterations = 1000
 
-    # Draw two lines using equation directly
+    # Draw circles using the midpoint algorithm
     vertices_midpoint = []
     begin = time.time()
     for i in range(iterations):
-        vertices_midpoint = draw_circle_midpoint(x, y, r)
+        vertices_midpoint = draw_circle_midpoint(x_c, y_c, r)
     end = time.time()
-    print("Time do draw " + str(iterations) + " circles using midpoint algorithm: " + str(end - begin))
+    print("Time to draw " + str(iterations) + " circles using the midpoint algorithm: " + str(end - begin))
 
     circle_midpoint = pyglet.graphics.vertex_list(int(len(vertices_midpoint)/2), ('v2f', vertices_midpoint))
     glColor3f(1, 0, 0)
--- draw_circle/src/draw_circle.cpp
+++ draw_circle/src/draw_circle.cpp
@@ -1,91 +1,148 @@
-#include <stdio.h>
+/**
+ * @file draw_circle.cpp
+ * @author Nguyen Quang <nguyenquang.emailbox@gmail.com>
+ * @brief The circle drawing algorithms in computer graphics.
+ * @since 0.0.1
+ * 
+ * @copyright Copyright (c) 2015, Nguyen Quang, all rights reserved.
+ * 
+ */
 
-#ifdef __linux__
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <sys/time.h>
 #include <GL/glut.h>
-#else
-#include <glut.h>
-#endif
 
-void importingData();
+/**
+ * @brief A struct to store 2D integer points.
+ * 
+ * @since 0.0.1
+ * 
+ */
+struct Point2i {
+    int x;
+    int y;
+
+    /**
+     * @brief Construct a new Point 2i object.
+     * 
+     * @param[in] x The x-coordinate.
+     * @param[in] y The y-coordinate.
+     * @since 0.0.1
+     */
+    Point2i(int x = 0, int y = 0) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+/**
+ * @brief Initialize opengl state.
+ * 
+ * @since 0.0.1
+ * 
+ */
 void initial();
-void circleMidPoint(void);
 
-int xC, yC;
-float R, p;
+/**
+ * @brief The openGL display function.
+ * 
+ * @since 0.0.1
+ * 
+ */
+void display();
 
+/**
+ * @brief The timer callback function.
+ * 
+ * @param[in] ms The time in minisecond.
+ * @since 0.0.1
+ */
+void timer(int ms);
+
+/**
+ * @brief Draw a circle using the midpoint algorithm.
+ * 
+ * @param[in] x_c The x-coordiante of the circle centre.
+ * @param[in] y_c The y-coordiante of the circle centre.
+ * @param[in] r The radius of the circle.
+ * @param[out] vertices The list of pixels need to be drawn.
+ * @since 0.0.1
+ */
+void draw_circle_midpoint(int x_c, int y_c, int r, std::vector<Point2i>& vertices);
+
+/**
+ * @brief The main function.
+ * 
+ * @param[in] argc The argument count.
+ * @param[in] argv The argument vector.
+ * @return The status value.
+ * @since 0.0.1
+ */
 int main(int argc, char** argv) {
-    importingData();
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(1352, 688);
+    glutInitWindowSize(640, 480);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("draw_circle");
     initial();
-    glutDisplayFunc(circleMidPoint);
+    timer(5);
+    glutDisplayFunc(display);
     glutMainLoop();
-    return 1;
-}
-
-void importingData() {
-    printf("Enter center point's coordinate.\n");
-    printf("xC = ");
-    if (scanf("%d", &xC) < 1) {
-        printf("Failed to enter xC.\n");
-    }
-    printf("yC = ");
-    if (scanf("%d", &yC) < 1) {
-        printf("Failed to enter yC.\n");
-    }
-    printf("Enter radius R = ");
-    if (scanf("%f", &R) < 1) {
-        printf("Failed to enter R.\n");
-    }
+    return 0;
 }
 
 void initial() {
-    glClearColor(1.0, 1.0, 1.0, 0.0);
-    glColor3f(1.0, 0.0, 0.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-676.0, 676.0, -344.0, 344.0, -1.0, 1.0);
+    gluOrtho2D(0, 640, 0, 480);
 }
 
-void circleMidPoint(void) {
-    p = 5 / 4 - R;
-    int x = 0;
-    float y = R;
+void display() {
     glClear(GL_COLOR_BUFFER_BIT);
+    int x_c = 300;
+    int y_c = 200;
+    int r = 150;
+    int iterations = 1000;
 
-    glBegin(GL_LINES);
-    {
-        glVertex2d(0, -344);
-        glVertex2d(0, 344);
-        glVertex2d(-676, 0);
-        glVertex2d(676, 0);
+    // Draw cirles using the midpoint algorithm
+    std::vector<Point2i> vertices_midpoint;
+    for (int i = 0; i < iterations; i++) {
+        draw_circle_midpoint(x_c, y_c, r, vertices_midpoint);
     }
-    glEnd();
 
-    glPointSize(2.0);
-    glColor3f(0.0, 0.0, 1.0);
-
+    glColor3f(1.0f, 0.0f, 0.0f);
     glBegin(GL_POINTS);
     {
-        glVertex2d(xC, yC);
+        for (int i = 0; i < vertices_midpoint.size(); i++) {
+            glVertex2d(vertices_midpoint[i].x, vertices_midpoint[i].y);
+        }
     }
     glEnd();
 
-    glBegin(GL_POINTS);
-    {
-        glVertex2d(xC + x, yC + y);
-        glVertex2d(xC + y, yC + x);
-        glVertex2d(xC + y, yC - x);
-        glVertex2d(xC + x, yC - y);
-        glVertex2d(xC - x, yC - y);
-        glVertex2d(xC - y, yC - x);
-        glVertex2d(xC - y, yC + x);
-        glVertex2d(xC - x, yC + y);
-    }
-    glEnd();
+    glFlush();
+}
+
+void timer(int ms) {
+    glutPostRedisplay();
+    glutTimerFunc(ms, timer, ms);
+}
+
+void draw_circle_midpoint(int x_c, int y_c, int r, std::vector<Point2i>& vertices) {
+    vertices.clear();
+    float p = 5 / 4 - r;
+    int x = 0;
+    int y = r;
+    vertices.push_back(Point2i(x_c + x, y_c + y));
+    vertices.push_back(Point2i(x_c + y, y_c + x));
+    vertices.push_back(Point2i(x_c + y, y_c - x));
+    vertices.push_back(Point2i(x_c + x, y_c - y));
+    vertices.push_back(Point2i(x_c - x, y_c - y));
+    vertices.push_back(Point2i(x_c - y, y_c - x));
+    vertices.push_back(Point2i(x_c - y, y_c + x));
+    vertices.push_back(Point2i(x_c - x, y_c + y));
 
     while (x < y) {
         if (p < 0) {
@@ -94,22 +151,14 @@ void circleMidPoint(void) {
             p = p + 2 * x - 2 * y + 5;
             y = y - 1;
         }
-
         x = x + 1;
-
-        glBegin(GL_POINTS);
-        {
-            glVertex2d(xC + x, yC + y);
-            glVertex2d(xC + y, yC + x);
-            glVertex2d(xC + y, yC - x);
-            glVertex2d(xC + x, yC - y);
-            glVertex2d(xC - x, yC - y);
-            glVertex2d(xC - y, yC - x);
-            glVertex2d(xC - y, yC + x);
-            glVertex2d(xC - x, yC + y);
-        }
-        glEnd();
+        vertices.push_back(Point2i(x_c + x, y_c + y));
+        vertices.push_back(Point2i(x_c + y, y_c + x));
+        vertices.push_back(Point2i(x_c + y, y_c - x));
+        vertices.push_back(Point2i(x_c + x, y_c - y));
+        vertices.push_back(Point2i(x_c - x, y_c - y));
+        vertices.push_back(Point2i(x_c - y, y_c - x));
+        vertices.push_back(Point2i(x_c - y, y_c + x));
+        vertices.push_back(Point2i(x_c - x, y_c + y));
     }
-
-    glFlush();
 }
